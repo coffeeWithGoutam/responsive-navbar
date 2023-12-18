a33313c1d073b896247eee59ae9f84cacc59d202
Azhar Ahmad <azhar@domnify.com>
2023-12-18 15:11:59 +0530

    update 1.0

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,7 +2,87 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div className="py-12">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-2/3 rounded  ">
+            <main className='p-4 bg-gray-100  shadow'>
+              <div className="flex flex-wrap -mx-4 -mb-10">
+                <div className="w-full md:w-1/2 mb-10 px-4">
+                  <div class="rounded-lg h-60 overflow-hidden">
+                    <img alt="content" class="object-cover object-top h-full w-full" src="https://dummyimage.com/1200x630" />
+                  </div>
+                  <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">The Catalyzer</h2>
+                  <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                  <div className="flex items-center mt-3">
+                    <img
+                      className="w-9 h-9 rounded-full mr-2"
+                      src="https://via.placeholder.com/40"
+                      alt="Avatar"
+                    />
+                    <p className="text-gray-600">Author Name</p>
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 mb-10 px-4">
+                  <div class="rounded-lg h-60 overflow-hidden">
+                    <img alt="content" class="object-cover object-top h-full w-full" src="https://dummyimage.com/1200x630" />
+                  </div>
+                  <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">The Catalyzer</h2>
+                  <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                  <div className="flex items-center mt-3">
+                    <img
+                      className="w-9 h-9 rounded-full mr-2"
+                      src="https://via.placeholder.com/40"
+                      alt="Avatar"
+                    />
+                    <p className="text-gray-600">Author Name</p>
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 mb-10 px-4">
+                  <div class="rounded-lg h-60 overflow-hidden">
+                    <img alt="content" class="object-cover object-top h-full w-full" src="https://dummyimage.com/1200x630" />
+                  </div>
+                  <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">The Catalyzer</h2>
+                  <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                  <div className="flex items-center mt-3">
+                    <img
+                      className="w-9 h-9 rounded-full mr-2"
+                      src="https://via.placeholder.com/40"
+                      alt="Avatar"
+                    />
+                    <p className="text-gray-600">Author Name</p>
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/2 mb-10 px-4">
+                  <div class="rounded-lg h-60 overflow-hidden">
+                    <img alt="content" class="object-cover object-top h-full w-full" src="https://dummyimage.com/1200x630" />
+                  </div>
+                  <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">The Catalyzer</h2>
+                  <p class="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
+                  <div className="flex items-center mt-3">
+                    <img
+                      className="w-9 h-9 rounded-full mr-2"
+                      src="https://via.placeholder.com/40"
+                      alt="Avatar"
+                    />
+                    <p className="text-gray-600">Author Name</p>
+                  </div>
+                </div>
+
+              </div>
+            </main>
+          </div>
+          <div className="w-full md:w-1/3">
+            <aside>
+            
+            </aside>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
