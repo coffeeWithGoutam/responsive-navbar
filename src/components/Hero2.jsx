a8d32304865cca475abf5142cfdb17de16b03cb4
Azhar Ahmad <azhar@domnify.com>
2023-12-26 14:21:24 +0530

    Position relative and Z-index Fixed

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import List from "../components/List";
-import Hero from "../assets/hero.svg";
+import HeroImg from "../assets/hero.svg";
 
 const Hero = () => {
   return (
-    <div>
-      <div className="flex flex-wrap justify-evenly items-center bg-[#2F3045] h-50v w-screen">
-        <div>
-          <h1 className="text-white font-bold text-4xl">
-            Best App Development Company
-            <br />
-            for Startups & Brands
-          </h1>
-          <List />
-          <button
-            onClick={() => console.log("Button clicked")}
-            className="bg-[#1D7EB9] text-white px-4 py-1.5 rounded hover:bg-blue-700 mt-9"
-          >
-            Talk to Our Expert
-          </button>
-        </div>
-        <div className="overflow-hidden p-5">
-          <img src={Hero} alt="Logo" className="h-72 rounded" />
+    <div className='bg-[#2f3045]'>
+      <div className="container mx-auto px-4 py-5 ">
+        <div className='flex flex-wrap justify-evenly items-center'>
+          <div>
+            <h1 className="text-white font-bold text-4xl">
+              Best App Development Company
+              <br />
+              for Startups & Brands
+            </h1>
+            <List />
+            <button
+              onClick={() => console.log("Button clicked")}
+              className="bg-[#1D7EB9] text-white px-4 py-1.5 rounded hover:bg-blue-700 mt-9"
+            >
+              Talk to Our Expert
+            </button>
+          </div>
+          <div className="p-5">
+            <img src={HeroImg} alt="Logo" className="h-72 rounded" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Ui.jsx
+++ src/components/Ui.jsx
@@ -1,12 +1,24 @@
 
 const Ui = () => {
   return (
-    <div className='bg-[#1D7EB9] h-60 w-screen flex flex-col items-center justify-center text-center text-white mt-14
-              md:px-6 md:py-8 md:text-lg'>
+    <div className='bg-[#1D7EB9] mt-14                md:px-6 md:py-8 md:text-lg'>
+      <div className="container mx-auto">
+        <div className="text-center">
         <h1 className='text-3xl mb-2'>Ready to Make an App and UX that stands out?</h1>
         <p className='text-sm md:text-base'>HASfit App rated 4.8/5 by more than 10 K reviewers while being used by 4 million users.</p>
         <button className='bg-white text-black px-4 py-1.5 rounded hover:bg-blue-700 hover:text-white mt-6 md:mt-8'>Talk to Our Expert</button>
+     
+        </div>
+
       </div>
+       
+     
+     
+      </div>
+
+
+
+
   )
 }
 
